8ce9f417f8b01f06efb78ff7e55f013894fc9087
Adam <chillcoders@gmail.com>
2022-12-13 18:06:29 -0500

    first3

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import Skeleton from "./components/skeleton";
+import Blogs from "./components/blogs";
+const App = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(" http://localhost:5000/blogs")
+        .then((response) => {
+          if (!response.ok) {
+            throw Error(
+              "Sorry, some error occurred while fetching your blogs."
+            );
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setLoading(false);
+          setError(false);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setError(true);
+        });
+    }, 4000);
+  });
+
+  return (
+    <div>
+      {blogs && <Blogs blogs={blogs} />}
+      <div className="container">
+        {loading && [1, 2, 3, 4, 5, 6, 7].map((n) => <Skeleton key={n} />)}
+      </div>
+      {error && (
+        <div className="container">
+          <span className="error">
+            Error connecting to the server. Connection failed.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ vite-project/src/components/skeleton.jsx
@@ -0,0 +1,17 @@
+const Skeleton = () => {
+  return (
+    <div className="skeleton-blog">
+      <header>
+        <div className="skeleton-avatar"></div>
+
+        <div className="skeleton-author"></div>
+      </header>
+
+      <main className="skeleton-image"></main>
+
+      <footer className="skeleton-footer"></footer>
+    </div>
+  );
+};
+
+export default Skeleton;
